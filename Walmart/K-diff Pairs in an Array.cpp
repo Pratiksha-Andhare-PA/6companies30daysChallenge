9ee9cb467964088cfaba35c8ca05aecda632e1ca
Pratiksha Andhare <128924098+Pratiksha-Andhare-PA@users.noreply.github.com>
2024-01-12 08:12:43 +0530

    Q4 Solution

--- Walmart/K-diff Pairs in an Array.cpp
+++ Walmart/K-diff Pairs in an Array.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
         set<pair<int, int>> s;
-        sort(nums.begin(), nums.end());
 
         int ans = 0;
         for(int i = 0; i < nums.size()-1; i++){
             for(int j = i+1; j < nums.size(); j++){
                 if(abs(nums[i] - nums[j]) == k){
-                    s.insert({nums[i], nums[j]});
+                    if(nums[i] > nums[j]){
+                        s.insert({nums[j], nums[i]});    
+                    }
+                    else{
+                        s.insert({nums[i], nums[j]});
+                    }
                 }
             }
         }
